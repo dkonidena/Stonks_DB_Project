8c4311e3ac1665d1772eba22a2d8af3f91d5bd3f
TFPAcer <acer78.minecraft@hotmail.co.uk>
2020-01-22 13:31:04 +0000

    initial commit

---
+++ inputFilter.js
@@ -0,0 +1,36 @@
+function setInputFilter(textbox, inputFilter) {
+  ["input", "keydown", "keyup", "mousedown", "mouseup", "select", "contextmenu", "drop"].forEach(function(event) {
+    textbox.addEventListener(event, function() {
+      if (inputFilter(this.value)) {
+        this.oldValue = this.value;
+        this.oldSelectionStart = this.selectionStart;
+        this.oldSelectionEnd = this.selectionEnd;
+      } else if (this.hasOwnProperty("oldValue")) {
+        this.value = this.oldValue;
+        this.setSelectionRange(this.oldSelectionStart, this.oldSelectionEnd);
+      } else {
+        this.value = "";
+      }
+    });
+  });
+}
+
+setInputFilter(document.getElementById("realPriceInput"), function(value) {
+  return /^\d*\.?\d*$/.test(value);
+});
+
+setInputFilter(document.getElementById("amountInput"), function(value) {
+  return /^\d*\.?\d*$/.test(value);
+});
+
+setInputFilter(document.getElementById("quantityInput"), function(value) {
+  return /^\d*\.?\d*$/.test(value);
+});
+
+setInputFilter(document.getElementById("strikeInput"), function(value) {
+  return /^\d*\.?\d*$/.test(value);
+});
+
+setInputFilter(document.getElementById("idInput"), function(value) {
+  return /^\d*$/.test(value);
+});
