300bbe1de77b11e47e0ebda5549c1a93c068c276
TFPAcer <acer78.minecraft@hotmail.co.uk>
2020-01-25 21:11:30 +0000

    fix tab/space issues

--- front-end/trades.js
+++ front-end/trades.js
@@ -1,48 +1,48 @@
 class Trade {
-	constructor(id) {
-		this.tradeId = id;
-		this.tradeDate = new Date();
-		this.userIdCreatedBy = 0;
-		this.lastModifiedDate = new Date();
+    constructor(id) {
+        this.tradeId = id;
+        this.tradeDate = new Date();
+        this.userIdCreatedBy = 0;
+        this.lastModifiedDate = new Date();
 
-		this.product = "";
-		this.buyingParty = "";
-		this.sellingParty = "";
-		this.quantity = 0;
-		this.notionalPrice = "";
-		this.notionalCurrency = "";
-		this.underlyingPrice = "";
-		this.underlyingCurrency = "";
-		this.maturityDate = new Date();
-		this.strikePrice = "";
-	}
+        this.product = "";
+        this.buyingParty = "";
+        this.sellingParty = "";
+        this.quantity = 0;
+        this.notionalPrice = "";
+        this.notionalCurrency = "";
+        this.underlyingPrice = "";
+        this.underlyingCurrency = "";
+        this.maturityDate = new Date();
+        this.strikePrice = "";
+    }
 };
 
 function addTrade(trade) {
-	let s = "<button class=\"btn trade-button d-block text-muted py-0 my-n1\"></button>";
-  	let b = $(s).text("Trade "+trade.tradeId).data("trade", trade);
-	b.on("click", () => {
-		loadTrade(trade);
-	})
-	let li = $("<li class=\"nav-item\"></li>").html(b);
-	$("#trades").append(li);
+    let s = "<button class=\"btn trade-button d-block text-muted py-0 my-n1\"></button>";
+      let b = $(s).text("Trade "+trade.tradeId).data("trade", trade);
+    b.on("click", () => {
+        loadTrade(trade);
+    })
+    let li = $("<li class=\"nav-item\"></li>").html(b);
+    $("#trades").append(li);
 };
 
 function loadTrade(trade) {
-	$("#tradeIdInput").val(trade.tradeId);
-	$("#tradeDateInput").val(trade.tradeDate);
-	$("#maturityDateInput").val(trade.maturityDate);
-	$("#quantityInput").val(trade.quantity);
-	$("#strikePriceInput").val(trade.strikePrice);
+    $("#tradeIdInput").val(trade.tradeId);
+    $("#tradeDateInput").val(trade.tradeDate);
+    $("#maturityDateInput").val(trade.maturityDate);
+    $("#quantityInput").val(trade.quantity);
+    $("#strikePriceInput").val(trade.strikePrice);
 }
 
 
 $("#addTradeButton").on("click", () => {
-	let id = ($("#trades").children().length + 1).toString().padStart(9, "0");
-	let trade = new Trade(id);
-	trade.tradeDate = new Date(1990, 0, 1, 0, 0, 0, 0);
-	trade.maturityDate = new Date(1990, 0, 1, 0, 0, 0, 0);
-	trade.quantity = 100;
-	trade.strikePrice = "320.20";
-	addTrade(trade);
+    let id = ($("#trades").children().length + 1).toString().padStart(9, "0");
+    let trade = new Trade(id);
+    trade.tradeDate = new Date(1990, 0, 1, 0, 0, 0, 0);
+    trade.maturityDate = new Date(1990, 0, 1, 0, 0, 0, 0);
+    trade.quantity = 100;
+    trade.strikePrice = "320.20";
+    addTrade(trade);
 });
