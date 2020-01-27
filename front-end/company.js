734b5fdd5ab84b0bbc93f74f5e21b2c9ffc5145b
TFPAcer <acer78.minecraft@hotmail.co.uk>
2020-01-27 18:31:13 +0000

    add company and product classes

---
+++ front-end/company.js
@@ -0,0 +1,9 @@
+class Company {
+    constructor() {
+        this.id = 0;
+        this.name = "";
+        this.foundedDate = new Date();
+        this.creationDate = new Date();
+        this.userIdCreatedBy = 0;
+    }
+};
---
+++ front-end/product.js
@@ -0,0 +1,11 @@
+class Product {
+    constructor() {
+        this.id = 0;
+        this.name = "";
+        this.companyId = 0;
+        this.value = 0;
+        this.creatioDate = new Date();
+        this.userIdCreatedBy = 0;
+    }
+
+};
