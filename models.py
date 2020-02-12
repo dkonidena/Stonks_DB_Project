5b29e7de62d99ba1ab537875ede57467ccfd9f21
Dhruva Konidena <44578390+dkonidena@users.noreply.github.com>
2020-02-12 20:17:55 +0000

    models have been given exceptions. need to handle resources.py

--- models.py
+++ models.py
@@ -1,10 +1,19 @@
 from run import db
 from datetime import datetime
-from sqlalchemy import ForeignKey, join, func, or_, Date, cast
-
+from sqlalchemy import ForeignKey, join, func, or_, Date, cast, exc
+import traceback
+import sys
 # expecting in YYYY-MM-DD HH:MM:SS
 def dateTruncate(dateString):
-    return datetime(int(dateString[0:4]), int(dateString[5:7]), int(dateString[8:10]), int(dateString[11:13]), int(dateString[14:16]), int(dateString[17:19])).strftime("%Y-%m-%d %H:%M:%S")
+    try:
+        return datetime(int(dateString[0:4]), int(dateString[5:7]), int(dateString[8:10]), int(dateString[11:13]), int(dateString[14:16]), int(dateString[17:19])).strftime("%Y-%m-%d %H:%M:%S")
+    except ValueError:
+        raise ValueError
+        print("Formatting of the String has caused an error")
+        traceback.print_exc(file=sys.stdout)
+    except:
+        print("Unknown Error occurred")
+        traceback.print_exc(file=sys.stdout)
 
 def returnDatetime(dateString):
     return datetime(int(dateString[0:4]), int(dateString[5:7]), int(dateString[8:10])).strftime("%Y-%m-%d")
@@ -20,23 +29,43 @@ class CompanyModel(db.Model):
     DateEnteredInSystem = db.Column(db.String(120))
 
     def save_to_db(self):
-        db.session.add(self)
-        db.session.commit()
+        try:
+            db.session.add(self)
+            db.session.commit()
+        except exc.IntegrityError:
+            raise exc.IntegrityError("","",1)
+            print("The Company couldn't be saved to the Database")
+            traceback.print_exc(file=sys.stdout)
+        except exc.InterfaceError:
+            raise exc.InterfaceError("","", 1)
+        except:
+            print("Unknown Error occurred")
+            traceback.print_exc(file=sys.stdout)
+        
 
     @classmethod
     def retrieve_companies_before(cls, date):
-        return cls.query.filter(func.DATE(CompanyModel.DateEnteredInSystem) <= returnDatetime(date))
+        try:
+            return cls.query.filter(func.DATE(CompanyModel.DateEnteredInSystem) <= returnDatetime(date))
+        except exc.ProgrammingError:
+            raise exc.ProgrammingError("","",1)
+            print("The number of parameters passed might not have been correct, please retry")
 
     @classmethod
     def update_company(cls, companycode, name, datefounded):
-        row = cls.query.filter_by(CompanyCode = companycode).first()
-        row.CompanyName = name
-        row.DateEnteredInSystem = datefounded
-        db.session.commit()
-
+        try:
+            row = cls.query.filter_by(CompanyCode = companycode).first()
+            row.CompanyName = name
+            row.DateEnteredInSystem = datefounded
+            db.session.commit()
+        except exc.IntegrityError:
+            raise exc.IntegrityError("","",1)
     @classmethod
     def delete_company(cls, companycode):
-        cls.query.filter_by(CompanyCode = companycode).delete()
+        try:
+            cls.query.filter_by(CompanyCode = companycode).delete()
+        except exc.IntegrityError:
+            raise exc.IntegrityError("","",1)
 
 class CurrencyTypesModel(db.Model):
     __tablename__ = 'CurrencyTypes'
@@ -45,8 +74,13 @@ class CurrencyTypesModel(db.Model):
     Country = db.Column(db.String(120), nullable = False)
 
     def save_to_db(self):
-        db.session.add(self)
-        db.session.commit()
+        try:
+            db.session.add(self)
+            db.session.commit()
+        except exc.IntegrityError:
+            raise exc.IntegrityError("","",1)
+        except exc.InterfaceError:
+            raise exc.InterfaceError("","", 1)
 
 class CurrencyValuationsModel(db.Model):
     __tablename__ = 'CurrencyValuations'
@@ -55,14 +89,21 @@ class CurrencyValuationsModel(db.Model):
     ValueInUSD = db.Column(db.Float, nullable = False)
 
     def save_to_db(self):
-        db.session.add(self)
-        db.session.commit()
+        try:
+            db.session.add(self)
+            db.session.commit()
+        except exc.IntegrityError:
+            raise exc.IntegrityError("","",1)
+        except exc.InterfaceError:
+            raise exc.InterfaceError("","", 1)
 
     @classmethod
     def retrieve_currency(cls, date):
-        print(get_date(date))
-        return cls.query.join(CurrencyTypesModel, cls.CurrencyCode == CurrencyTypesModel.CurrencyCode).filter(cls.DateOfValuation.like(get_date(date)+"%")).all()
-
+        try:
+            print(get_date(date))
+            return cls.query.join(CurrencyTypesModel, cls.CurrencyCode == CurrencyTypesModel.CurrencyCode).filter(cls.DateOfValuation.like(get_date(date)+"%")).all()
+        except exc.ProgrammingError:
+            raise exc.ProgrammingError("","",1)
 
 class DerivativeTradesModel(db.Model):
     __tablename__ = 'DerivativeTrades'
@@ -83,56 +124,83 @@ class DerivativeTradesModel(db.Model):
     # DateLastModified = db.Column(db.String(120), nullable = False)
 
     def save_to_db(self):
-        db.session.add(self)
-        db.session.commit()
-        return self.TradeID
+        try:
+            db.session.add(self)
+            db.session.commit()
+            return self.TradeID
+        except exc.IntegrityError:
+            raise exc.IntegrityError("","",1)
+        except exc.InterfaceError:
+            raise exc.InterfaceError("","", 1)
 
     @classmethod
     def get_trades_between(cls, startDate, endDate):
-        return cls.query.filter(func.DATE(DerivativeTradesModel.DateOfTrade) >= returnDatetime(startDate), func.DATE(DerivativeTradesModel.DateOfTrade) <= returnDatetime(endDate))
-
+        try:
+            return cls.query.filter(func.DATE(DerivativeTradesModel.DateOfTrade) >= returnDatetime(startDate), func.DATE(DerivativeTradesModel.DateOfTrade) <= returnDatetime(endDate))
+        except exc.ProgrammingError:
+            raise exc.ProgrammingError("", "", 1)
     @classmethod
     def get_trade_with_ID(cls, tradeID):
-        return cls.query.filter(DerivativeTradesModel.TradeID == tradeID)
+        try:
+            return cls.query.filter(DerivativeTradesModel.TradeID == tradeID)
+        except exc.ProgrammingError:
+            raise exc.ProgrammingError("", "", 1)
 
     @classmethod
     def get_trades_sold_by(cls, sellingParty):
-        return cls.query.filter(DerivativeTradesModel.SellingParty == sellingParty)
-
+        try:
+            return cls.query.filter(DerivativeTradesModel.SellingParty == sellingParty)
+        except exc.ProgrammingError:
+            raise exc.ProgrammingError("", "", 1)
     @classmethod
     def get_trades_bought_by(cls, buyingParty):
-        return cls.query.filter(DerivativeTradesModel.BuyingParty == buyingParty)
+        try:
+            return cls.query.filter(DerivativeTradesModel.BuyingParty == buyingParty)
+        except exc.ProgrammingError:
+            raise exc.ProgrammingError("", "", 1)
 
     @classmethod
     def get_trade_by_product(cls, productName):
-        return cls.query.filter(ProductModel.ProductName == productName, ProductModel.ProductID == ProductTradesModel.ProductID, ProductTradesModel.TradeID == cls.TradeID)
-
+        try:
+            return cls.query.filter(ProductModel.ProductName == productName, ProductModel.ProductID == ProductTradesModel.ProductID, ProductTradesModel.TradeID == cls.TradeID)
+        except exc.ProgrammingError:
+            raise exc.ProgrammingError("", "", 1)
     @classmethod
     def get_trades_by_notional_currency(cls, notionalCurrency):
-        return cls.query.filter(DerivativeTradesModel.NotionalCurrency == notionalCurrency)
-
+        try:
+            return cls.query.filter(DerivativeTradesModel.NotionalCurrency == notionalCurrency)
+        except exc.ProgrammingError:
+            raise exc.ProgrammingError("", "", 1)
     @classmethod
     def get_trade_by_underlying_currency(cls, underlyingCurrency):
-        return cls.query.filter(DerivativeTradesModel.TradeID == underlyingCurrency)
-
+        try:
+            return cls.query.filter(DerivativeTradesModel.TradeID == underlyingCurrency)
+        except exc.ProgrammingError:
+            raise exc.ProgrammingError("", "", 1)
     @classmethod
     def get_trades_by_user(cls, userID):
-        return cls.query.filter(DerivativeTradesModel.UserIDCreatedBy == userID)
-
+        try:
+            return cls.query.filter(DerivativeTradesModel.UserIDCreatedBy == userID)
+        except exc.ProgrammingError:
+            raise exc.ProgrammingError("", "", 1)
     @classmethod
     def update_trade(cls, tradeID, product, buyingParty, sellingParty, notionalValue, notionalCurrency, quantity, maturityDate, underlyingValue, underlyingCurrency, strikePrice):
-        row = cls.query.filter_by(TradeID = tradeID).first()
-        row.Product = product
-        row.BuyingParty = buyingParty
-        row.SellingParty = sellingParty
-        row.NotionalValue = notionalValue
-        row.NotionalCurrency = notionalCurrency
-        row.Quantity = quantity
-        row.MaturityDate = maturityDate
-        row.UnderlyingValue = underlyingValue
-        row.UnderlyingCurrency = underlyingCurrency
-        row.StrikePrice = strikePrice
-        db.session.commit()
+        try:
+            row = cls.query.filter_by(TradeID = tradeID).first()
+            row.Product = product
+            row.BuyingParty = buyingParty
+            row.SellingParty = sellingParty
+            row.NotionalValue = notionalValue
+            row.NotionalCurrency = notionalCurrency
+            row.Quantity = quantity
+            row.MaturityDate = maturityDate
+            row.UnderlyingValue = underlyingValue
+            row.UnderlyingCurrency = underlyingCurrency
+            row.StrikePrice = strikePrice
+            db.session.commit()
+        except exc.IntegrityError:
+           raise exc.IntegrityError("", "", 1)
+
 
 class EventLogModel(db.Model):
     __tablename__ = 'EventLog'
@@ -142,12 +210,21 @@ class EventLogModel(db.Model):
     EmployeeID = db.Column(db.String(120), ForeignKey("Employees.EmployeeID"), nullable = False)
 
     def save_to_db(self):
-        db.session.add(self)
-        db.session.commit()
+        try:
+            db.session.add(self)
+            db.session.commit()
+        except exc.IntegrityError:
+            raise exc.IntegrityError("","",1)
+        except exc.InterfaceError:
+            raise exc.InterfaceError("","", 1)
 
     @classmethod
     def get_events(cls, date): #gets all the events/actions made by users on a specific date
-        return cls.query.filter(cls.DateOfEvent <= date).with_entities(EventLogModel.DateOfEvent, EventLogModel.UserAction)
+        try:
+            return cls.query.filter(cls.DateOfEvent <= date).with_entities(EventLogModel.DateOfEvent, EventLogModel.UserAction)
+        except exc.ProgrammingError:
+            raise exc.ProgrammingError("", "", 1)
+
 
 class EmployeesModel(db.Model):
     __tablename__ = 'Employees'
@@ -158,8 +235,13 @@ class EmployeesModel(db.Model):
     # Password = db.Column(db.String(120), nullable = False)
 
     def save_to_db(self):
-        db.session.add(self)
-        db.session.commit()
+        try:
+            db.session.add(self)
+            db.session.commit()
+        except exc.IntegrityError:
+            raise exc.IntegrityError("","",1)
+        except exc.InterfaceError:
+            raise exc.InterfaceError("","", 1)
 
 class ProductSellersModel(db.Model):
     __tablename__ = 'ProductSellers'
@@ -167,13 +249,20 @@ class ProductSellersModel(db.Model):
     CompanyCode = db.Column(db.String(120), primary_key = True,  nullable = False)
 
     def save_to_db(self):
-        db.session.add(self)
-        db.session.commit()
+        try:
+            db.session.add(self)
+            db.session.commit()
+        except exc.IntegrityError:
+            raise exc.IntegrityError("","",1)
+        except exc.InterfaceError:
+            raise exc.InterfaceError("","", 1)
 
     @classmethod
     def getProductID(cls, productName, companyCode):
-        print(cls.query.filter(ProductSellersModel.CompanyCode == companyCode).filter(ProductSellersModel.ProductID == ProductModel.ProductID).filter(ProductModel.ProductName == productName).with_entities(ProductSellersModel.ProductID))
-        return cls.query.filter(ProductSellersModel.CompanyCode == companyCode).filter(ProductSellersModel.ProductID == ProductModel.ProductID).filter(ProductModel.ProductName == productName).with_entities(ProductSellersModel.ProductID)
+        try:
+            return cls.query.filter(ProductSellersModel.CompanyCode == companyCode).filter(ProductSellersModel.ProductID == ProductModel.ProductID).filter(ProductModel.ProductName == productName).with_entities(ProductSellersModel.ProductID)
+        except exc.ProgrammingError:
+            raise exc.ProgrammingError("", "", 1)
 
 class ProductTradesModel(db.Model):
     __tablename__ = 'ProductTrades'
@@ -181,8 +270,13 @@ class ProductTradesModel(db.Model):
     ProductID = db.Column(db.Integer, primary_key = True,  nullable = False)
 
     def save_to_db(self):
-        db.session.add(self)
-        db.session.commit()
+        try:
+            db.session.add(self)
+            db.session.commit()
+        except exc.IntegrityError:
+            raise exc.IntegrityError("","",1)
+        except exc.InterfaceError:
+            raise exc.InterfaceError("","", 1)
 
 class ProductModel(db.Model):
     __tablename__ = 'Products'
@@ -191,14 +285,22 @@ class ProductModel(db.Model):
     DateEnteredInSystem = db.Column(db.String(120))
 
     def save_to_db(self):
-        db.session.add(self)
-        db.session.commit()
-        return self.ProductID
+        try:
+            db.session.add(self)
+            db.session.commit()
+            return self.ProductID
+        except exc.IntegrityError:
+            raise exc.IntegrityError("","",1)
+        except exc.InterfaceError:
+            raise exc.InterfaceError("","", 1)
 
     @classmethod
     def retrieve_products_on_date(cls, date):
-        return cls.query.filter(cls.ProductID == ProductSellersModel.ProductID, ProductSellersModel.ProductID == ProductValuationsModel.ProductID, func.DATE(cls.DateEnteredInSystem) == returnDatetime(date)).\
+        try:
+            return cls.query.filter(cls.ProductID == ProductSellersModel.ProductID, ProductSellersModel.ProductID == ProductValuationsModel.ProductID, func.DATE(cls.DateEnteredInSystem) == returnDatetime(date)).\
             with_entities(ProductModel.ProductID, ProductModel.ProductName, ProductSellersModel.CompanyCode, ProductValuationsModel.ProductPrice)
+        except exc.ProgrammingError:
+            raise exc.ProgrammingError("", "", 1)
 
 class ProductValuationsModel(db.Model):
     __tablename__ = 'ProductValuations'
@@ -207,5 +309,10 @@ class ProductValuationsModel(db.Model):
     DateOfValuation = db.Column(db.String(120), primary_key = True, nullable = False)
 
     def save_to_db(self):
-        db.session.add(self)
-        db.session.commit()
+        try:
+            db.session.add(self)
+            db.session.commit()
+        except exc.IntegrityError:
+            raise exc.IntegrityError("","",1)
+        except exc.InterfaceError:
+            raise exc.InterfaceError("","", 1)
