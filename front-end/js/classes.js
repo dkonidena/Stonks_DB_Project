f1d94acd1dca8fe93ca2b2d6f12a55e9b94514cf
Daniel <danielfspencer1@gmail.com>
2020-02-19 19:58:46 +0000

    Revert "Partial fix for filters"
    This reverts commit febc46900129f60b16e04ac6a544a957d5c8e0aa.

--- front-end/js/classes.js
+++ front-end/js/classes.js
@@ -249,7 +249,7 @@ function getCompanyList(date, order, res) {
 
 class Filter {
     constructor() {
-        this.dateCreated = new Date();
+        this.dateCreated = null;
     }
 
     getAPIObject() {
@@ -271,7 +271,7 @@ class Filter {
 class TradeFilter extends Filter {
     constructor() {
         super();
-        this.dateModified = new Date();
+        this.dateModified = null;
         this.tradeID = null;
         this.buyingParty = null;
         this.sellingParty = null;
