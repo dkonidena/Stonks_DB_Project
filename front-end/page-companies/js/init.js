976623d5c630735f21ac99c54e6e304cdfa2a8be
Daniel <danielfspencer1@gmail.com>
2020-02-20 16:59:57 +0000

    Add inital JS scripts

---
+++ front-end/page-companies/js/init.js
@@ -0,0 +1,15 @@
+const filters = [
+    ["#filter-creationDateLowerDayInput", /^\d{0,2}$/],
+    ["#filter-creationDateLowerMonthInput", /^\d{0,2}$/],
+    ["#filter-creationDateLowerYearInput", /^\d{0,4}$/],
+    ["#filter-creationDateUpperDayInput", /^\d{0,2}$/],
+    ["#filter-creationDateUpperMonthInput", /^\d{0,2}$/],
+    ["#filter-creationDateUpperYearInput", /^\d{0,4}$/],
+];
+
+$(document).ready(() => {
+    filters.forEach((x) => {
+        var t = x[0];
+        setInputFilter(t, (v) => { return x[1].test(v) });
+    });
+});
