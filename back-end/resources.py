45e163fd9d50863fabf163508d0805fb474dca6f
RSAMUEL12 <rohansamm@users.noreply.github.com>
2020-03-02 21:25:50 +0000

    Changed some of the code for back-end error handling

--- back-end/resources.py
+++ back-end/resources.py
@@ -336,9 +336,9 @@ class Products(Resource):
             company_ID = data['companyID']
             if len(name) == 0:
                 return {'message':'product name is empty'}, 400
-            if len(value) == 0:
+            if len(value_in_USD) == 0:
                 return {'message':'product value is empty'}, 400
-            if len(companyCode) == 0:
+            if len(company_ID) == 0:
                 return {'message':'company code is empty'}, 400
             date_now = str(date_func.today())
             models.ProductModel.update_product(product_ID, name)
@@ -625,7 +625,7 @@ class Trades(Resource):
             trade_ID = request.args.get('id')
             if 'id' not in request.args:
                 return {'message': 'Request malformed'}, 400
-            if models.DerivativeTradesModel.get_trade_with_ID(trade_ID) == None:
+            if models.DerivativeTradesModel.get_trade_with_ID(trade_ID).count() == 0:
                 return {'message': 'Trade id not present'}, 400
             models.DerivativeTradesModel.delete_trade(trade_ID)
             userAction = "User has deleted a record in the Trades table with the ID: " + trade_ID
@@ -662,17 +662,20 @@ class Reports(Resource):
                         tradeDates = models.DerivativeTradesModel.get_trade_dates_after(filter['dateCreated']['after'])
                     else:
                         tradeDates = models.DerivativeTradesModel.get_trade_dates_before(filter['dateCreated']['before'])
+                    noOfMatches = tradeDates.count()
                 elif len(filter['dateCreated']) == 2:
                     tradeDates = models.DerivativeTradesModel.get_trade_dates_between(filter['dateCreated']['after'], filter['dateCreated']['before'])
+                    noOfMatches = tradeDates.count()
                 else:
                     tradeDates = models.DerivativeTradesModel.get_all_trade_dates()
+                    noOfMatches = len(tradeDates)
             else:
                 tradeDates = models.DerivativeTradesModel.get_all_trade_dates()
+                noOfMatches = len(tradeDates)
 
             for each in tradeDates:
                 result = models.DerivativeTradesModel.get_trades_between(each.DateOfTrade, each.DateOfTrade)
                 results.append(result)
-            noOfMatches = len(results)
 
             # contents for the pdf file
             story = [] # all relevant data for the pdf
